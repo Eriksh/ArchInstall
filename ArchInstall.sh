1f9495b13acbc2357f2b418e804997f8a784c90e
Erik <ehall.3624@gmail.com>
2016-12-11 14:17:24 -0800

    expanding installer

--- ArchInstall.sh
+++ ArchInstall.sh
@@ -63,34 +63,43 @@ request_new_user_password="yes"
 
 
 
-################################################################################
-################################################################################
+
 ################################################################################
 # DO NOT TOUCH ANYTHING BELOW THIS LINE
 ################################################################################
-################################################################################
-################################################################################
-
 
 #############################################################
 # Create Seperate Config File
 #############################################################
-echo "OS_packages=\""$OS_packages"\"" > /mnt/ArchConfig.sh
-echo "OS_name=\""$OS_name"\"" >> /mnt/ArchConfig.sh
-echo "locale=\""$locale"\"" >> /mnt/ArchConfig.sh
-echo "timezone_region=\""$timezone_region"\"" >> /mnt/ArchConfig.sh
-echo "timezone_city=\""$timezone_city"\"" >> /mnt/ArchConfig.sh
-echo "request_new_root_password=\""$request_new_root_password"\"" >> /mnt/ArchConfig.sh
-echo "mirrorlist_country=\""$mirrorlist_country"\"" >> /mnt/ArchConfig.sh
-echo "mirrorlist_protocol=\""$mirrorlist_protocol"\"" >> /mnt/ArchConfig.sh
-echo "rank_mirrorlist_by=\""$rank_mirrorlist_by"\"" >> /mnt/ArchConfig.sh
-echo "repository=\""$repository"\"" >> /mnt/ArchConfig.sh
-echo "install_antivirus=\""$install_antivirus"\"" >> /mnt/ArchConfig.sh
-echo "install_firewall=\""$install_firewall"\"" >> /mnt/ArchConfig.sh
-echo "install_firejail=\""$install_firejail"\"" >> /mnt/ArchConfig.sh
-echo "usernames=("$usernames")" >> /mnt/ArchConfig.sh
-echo "sudo_update_users=("$sudo_update_users")" >> /mnt/ArchConfig.sh
-echo "request_new_user_password=\""$request_new_user_password"\"" >> /mnt/ArchConfig.sh
+echo "OS_packages=\""$OS_packages"\"" > /mnt/config.sh
+echo "OS_name=\""$OS_name"\"" >> /mnt/config.sh
+echo "locale=\""$locale"\"" >> /mnt/config.sh
+echo "timezone_region=\""$timezone_region"\"" >> /mnt/config.sh
+echo "timezone_city=\""$timezone_city"\"" >> /mnt/config.sh
+echo "request_new_root_password=\""$request_new_root_password"\"" >> /mnt/config.sh
+echo "mirrorlist_country=\""$mirrorlist_country"\"" >> /mnt/config.sh
+echo "mirrorlist_protocol=\""$mirrorlist_protocol"\"" >> /mnt/config.sh
+echo "rank_mirrorlist_by=\""$rank_mirrorlist_by"\"" >> /mnt/config.sh
+echo "repository=\""$repository"\"" >> /mnt/config.sh
+echo "install_antivirus=\""$install_antivirus"\"" >> /mnt/config.sh
+echo "install_firewall=\""$install_firewall"\"" >> /mnt/config.sh
+echo "install_firejail=\""$install_firejail"\"" >> /mnt/config.sh
+echo "usernames=("$usernames")" >> /mnt/config.sh
+echo "sudo_update_users=("$sudo_update_users")" >> /mnt/config.sh
+echo "request_new_user_password=\""$request_new_user_password"\"" >> /mnt/config.sh
 
-chmod +x /mnt/ArchConfig.sh
+chmod +x /mnt/config.sh
 echo "Created new config file..."
+
+#############################################################
+# Download Installation Files
+#############################################################
+cd /mnt
+wget https://raw.githubusercontent.com/Eriksh/ArchInstall/develop/installOS.sh && chmod +x installOS.sh
+wget https://raw.githubusercontent.com/Eriksh/ArchInstall/develop/configOS.sh && chmod +x configOS.sh
+
+#############################################################
+# Run Install Script
+#############################################################
+cd /mnt
+./installOS
